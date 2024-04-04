717ae53edf50c00823e5279b6c3d870d369dc494
jerperez42 <jerperez@student.42.fr>
2024-04-04 13:22:19 +0200

    print correct values

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jerperez <jerperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:47:22 by jerperez          #+#    #+#             */
-/*   Updated: 2024/02/07 17:55:00 by jerperez         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:20:11 by jerperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	std::cout << "&str:\t\t" << &str << std::endl;
-	std::cout << "&stringPTR:\t" << &stringPTR << std::endl;
+	std::cout << "stringPTR:\t" << stringPTR << std::endl;
 	std::cout << "&stringREF:\t" << &stringREF << std::endl;
 	std::cout << "str:\t\t" << str << std::endl;
 	std::cout << "*stringPTR:\t" << *stringPTR << std::endl;
