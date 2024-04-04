5c1a45fadc1c406773c185e612ac6a0ed660b648
jerperez42 <jerperez@student.42.fr>
2024-04-04 13:21:28 +0200

    update

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: jerperez <jerperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 15:49:20 by jerperez          #+#    #+#             */
-/*   Updated: 2024/04/04 11:17:52 by jerperez         ###   ########.fr       */
+/*   Updated: 2024/04/04 13:15:08 by jerperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ Zombie*	zombieHorde(int N, std::string name)
 {
 	Zombie	*horde = new Zombie[N];
 
-	horde[0] = Zombie(name);
-	for (int i = 1; i < N; i++)
-		horde[i] = horde[0];
+	for (int i = 0; i < N; i++)
+		horde[i].setName(name);
 	return (horde);
 }
